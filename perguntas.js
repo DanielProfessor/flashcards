f614de69bb2493b0599e7ecd2066463addc390ce
DanielProfessor <167529251+DanielProfessor@users.noreply.github.com>
2024-10-31 19:33:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+    criaCartao(
+        'Matemática',
+        'Qual é a soma de 12 e 15?',
+        'A soma de 12 e 15 é 27'
+    )
+    
+    criaCartao(
+        'Matemática',
+        'Qual é a raiz quadrada de 144?',
+        'A raiz quadrada de 144 é 12'
+    )
+    
+    criaCartao(
+        'Matemática',
+        'O que é um número primo?',
+        'Um número primo é um número maior que 1 que tem apenas dois divisores: 1 e ele mesmo'
+    )
+    
+    criaCartao(
+        'Matemática',
+        'Qual é o produto de 9 e 6?',
+        'O produto de 9 e 6 é 54'
+    )
+    
+    criaCartao(
+        'Matemática',
+        'Qual é a diferença entre 50 e 18?',
+        'A diferença entre 50 e 18 é 32'
+    )
+    
+    criaCartao(
+        'Matemática',
+        'Qual é a fórmula da área de um círculo?',
+        'A fórmula da área de um círculo é A = πr²'
+    )
+    
